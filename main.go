a0da7dc475d65f11c0e61bcef21489898883e6d2
Parham Alvani <parham.alvani@gmail.com>
2025-06-14 01:42:01 +0000

    feat: tests using map

--- main.go
+++ main.go
@@ -13,4 +13,25 @@ func main() {
 	} else {
 		fmt.Printf("Test 1: Failed %s\n", message)
 	}
+
+	result, message = pkg.ForList([]int{22, 33, 44}).ElemAt(1).ShouldBe(pkg.EqualToFunc(33))
+	if result {
+		fmt.Println("Test 1: Success")
+	} else {
+		fmt.Printf("Test 1: Failed %s\n", message)
+	}
+
+	result, message = pkg.ForMap(map[string]int{"k1": 22, "k2": 33, "k3": 44}).ElemAt("k2").ShouldBe(pkg.EqualToFunc(33))
+	if result {
+		fmt.Println("Test 1: Success")
+	} else {
+		fmt.Printf("Test 1: Failed %s\n", message)
+	}
+
+	result, message = pkg.ForMap(map[string]int{"k1": 22, "k2": 33, "k3": 44}).ElemAt("k2").ShouldBe(pkg.EqualToFunc(3))
+	if result {
+		fmt.Println("Test 1: Success")
+	} else {
+		fmt.Printf("Test 1: Failed %s\n", message)
+	}
 }

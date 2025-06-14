65ba17853b7369deb8b415fe3b60e0bbd8a373f5
Parham Alvani <parham.alvani@gmail.com>
2025-06-14 01:33:51 +0000

    feat: add map values

--- pkg/value.go
+++ pkg/value.go
@@ -6,18 +6,12 @@ type Value[T any] struct {
 	v T
 }
 
+func (v Value[T]) ShouldBe(cnd Condition[T]) (bool, string) {
+	return cnd.Check(v)
+}
+
 type Values[T any] struct {
 	v []T
-}
-
-type ValuesMap[K comparable, V any] struct {
-	v map[K]V
-}
-
-func ForList[T any](v []T) Values[T] {
-	return Values[T]{
-		v: v,
-	}
 }
 
 func (v Values[T]) ElemAt(index int) Value[T] {
@@ -30,6 +24,28 @@ func (v Values[T]) ElemAt(index int) Value[T] {
 	}
 }
 
-func (v Value[T]) ShouldBe(cnd Condition[T]) (bool, string) {
-	return cnd.Check(v)
+func ForList[T any](v []T) Values[T] {
+	return Values[T]{
+		v: v,
+	}
+}
+
+type ValuesMap[K comparable, V any] struct {
+	v map[K]V
+}
+
+func (v ValuesMap[K, V]) ElemAt(key K) Value[V] {
+	if _, ok := v.v[key]; !ok {
+		panic(fmt.Sprintf("there is no element with key equal to %v", key))
+	}
+
+	return Value[V]{
+		v: v.v[key],
+	}
+}
+
+func ForMap[K comparable, V any](v map[K]V) ValuesMap[K, V] {
+	return ValuesMap[K, V]{
+		v: v,
+	}
 }
